e38d62174704c71e066188abf827942469823c78
Kenref <107287396+Kenref@users.noreply.github.com>
2023-07-25 20:52:02 +1000

    changed settings

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -1,35 +1,23 @@
 // import { useState } from 'react'
 
-
 function App() {
+    return (
+        <>
+            <div className="left-side">
+                <div>
+                    <h3>About section</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repudiandae labore a ipsum.
+                        Facere ea, voluptatem animi minus ipsum deleniti ullam
+                    </p>
+                </div>
 
-  return (
-	<>
-		<div className="left-side">
-			<div>
-			<h3>About section</h3>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repudiandae labore a ipsum. Facere ea, voluptatem animi minus ipsum deleniti ullam</p>
-		</div>
+                <div></div>
+            </div>
 
-		<div>
-
-		</div>
-
-
-
-
-
-
-
-  </div>
-
-  <div className="right-side">
-
-  </div>
-	</>
-  )
-
-
+            <div className="right-side"></div>
+        </>
+    );
 }
 
-export default App
+export default App;
